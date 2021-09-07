a16a936316096070b9d6c1f7753a78c76584856a
Bjørn Dybvik Langfors <bdl@digdir.no>
2021-09-07 11:26:06 +0200

    Initial commit

---
+++ samples/SampleWebApp/Services/MyDanClientSecretService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Altinn.ApiClients.Dan.Models;
+using Altinn.ApiClients.Maskinporten.Config;
+using Altinn.ApiClients.Maskinporten.Models;
+using Altinn.ApiClients.Maskinporten.Service;
+using Microsoft.Extensions.Options;
+using SampleWebApp.Service;
+
+namespace SampleWebApp.Services
+{
+    public class MyDanClientSecretService : IClientSecret<IMyDanClientSecretService>
+    {
+        private DanOptions _danOptions;
+
+        public MyDanClientSecretService(IOptions<DanOptions> danOptions)
+        {
+            _danOptions = danOptions.Value;
+        }
+
+        public Task<ClientSecrets> GetClientSecrets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Altinn.ApiClients.Dan/Models/DanOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.ApiClients.Dan.Models
+{
+    public class DanOptions
+    {
+        public string SubscriptionKey { get; set; }
+        public string ClientId { get; set; }
+        public string Scopes { get; set; }
+    }
+}
