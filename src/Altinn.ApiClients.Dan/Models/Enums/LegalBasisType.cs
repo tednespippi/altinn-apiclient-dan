bae56c17d442bad95eac1e320c9aff706627d925
Bjørn Dybvik Langfors <bdl@digdir.no>
2022-05-11 19:37:44 +0200

    Added CPV type

--- src/Altinn.ApiClients.Dan/Models/Enums/LegalBasisType.cs
+++ src/Altinn.ApiClients.Dan/Models/Enums/LegalBasisType.cs
@@ -3,13 +3,18 @@
 namespace Altinn.ApiClients.Dan.Models.Enums
 {
     /// <summary>
-    /// The type of legal basis, usually. ESPD
+    /// The type of legal basis
     /// </summary>
     public enum LegalBasisType
     {
         /// <summary>
         /// Legal basis is a ESPD document
         /// </summary>
-        [EnumMember(Value = "ESPD")] Espd = 1
+        [EnumMember(Value = "ESPD")] Espd = 1,
+
+        /// <summary>
+        /// CPV code use for procurements
+        /// </summary>
+        [EnumMember(Value = "cpv")] Cpv = 2,
     }
 }
